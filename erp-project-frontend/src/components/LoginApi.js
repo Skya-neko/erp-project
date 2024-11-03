3f4c9dd104a2c6b72f02624708e292c5da903bcd
Violet <daby6789@gmail.com>
2024-11-03 15:27:08 +0800

    新增登入API

---
+++ erp-project-frontend/src/components/LoginApi.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+
+export const execLogin = async (username, password) => {
+    const url = `${process.env.REACT_APP_AUTH_API_URL}`;
+
+    const header = {
+        "Content-Type": "application/json",
+    };
+
+    const data = {
+        clientId: username,
+        clientSecret: password
+    };
+
+    return await axios({
+        method: "POST",
+        url: url,
+        headers: header,
+        data: data,
+    })
+        .then((res) => {
+            if (res.data.code === "00") {
+                console.log(`res: ${JSON.stringify(res)}`);
+                return res.data;
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+};
